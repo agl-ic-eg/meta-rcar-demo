e6e5be900f2f99fc437b38e3d6db446848a218ef
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-02-21 15:50:33 +0900

    Revert "script: Use pigz instead of gz if pigz is installed"
    This reverts commit 2dad0840c67cf83e6adc0786c0dcc214ec597e3b.

--- build_xen_doma_agl_cluster.sh
+++ build_xen_doma_agl_cluster.sh
@@ -85,12 +85,8 @@ find yocto/common_data/sstate | grep xen: | xargs rm -r
 find yocto/common_data/sstate | grep arm-trusted-firmware: | xargs rm -r
 
 ninja
-if [[ "$(pigz >/dev/null 2>&1; echo $?)" -eq 0 ]]; then
-    ninja image-full
-    pigz -k full.img
-else
-    ninja full.img.gz
-fi
+ninja full.img.gz
+
 mkdir -p firmware
 find ./yocto/build-domd/tmp/deploy/images/ -name "*.srec" | xargs cp -t firmware
 
